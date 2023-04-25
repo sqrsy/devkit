f7bc39ffa41e75f54f2d2897dfe967dda395a937
Fair & Square Synthesis <thom@sqrsy.com>
2023-04-25 12:01:21 +1000

    Stage tested example of Rasa 3x2 hardware

---
+++ examples/example_rasa3x2/Envelope.h
@@ -0,0 +1,109 @@
+class Envelope {
+  public:
+
+    // gate used to manage sustain
+    int gate_on = false;
+    bool pause_DR = false;
+
+    // size, min, max, and sustain in mV
+    int current_size;  // the current size of envelope to send to analog out
+    int current_limit;
+    int max_limit = 4000;
+    int sustain_limit;
+    int min_limit = 0;
+
+    // how many milliseconds before changing voltage
+    int rate_A;
+    int rate_DR;  // assume decay / release have same rate
+
+    bool fast_A = false;         // whether to use fast attack
+    bool fast_DR = false;        // whether to use fast decay / release
+    bool in_progress_A = false;  // if attack is in progress
+    bool in_progress_DR = false; // if decay / release is in progress
+
+    void turn_on_gate() {
+      gate_on = true;
+      in_progress_A = true;
+      in_progress_DR = false;
+      current_size = min_limit; // reset envelope at bottom
+      pause_DR = false;
+      reset_timer();
+    }
+
+    void turn_off_gate() {
+      gate_on = false;
+      pause_DR = false; // now that gate is off, unpause envelope to start release
+      reset_timer();
+    }
+
+    long int last_timer = 0;
+    void reset_timer() {
+      last_timer = millis();
+    }
+
+    long int get_timer() {
+      return (millis() - last_timer);
+    }
+
+    void rise() {
+
+      if (rate_A == 0) {
+        current_size = max_limit;
+      } else {
+        if (get_timer() > rate_A) {
+          if (fast_A) {
+            current_size += .05 * max_limit;
+          } else {
+            current_size += .01 * max_limit;
+          }
+          reset_timer();
+        }
+      }
+      if (current_size >= max_limit) {
+        current_size = max_limit;
+        in_progress_A = false;
+        in_progress_DR = true;
+      }
+    }
+
+    void fall() {
+
+      if (gate_on) {
+        current_limit = sustain_limit;
+      } else {
+        current_limit = min_limit;
+      }
+
+      if (!pause_DR) {
+        if (rate_DR == 0) {
+          current_size = current_limit;
+        } else {
+          if (get_timer() > rate_DR) {
+            if (fast_DR) {
+              current_size -= .05 * max_limit;
+            } else {
+              current_size -= .01 * max_limit;
+            }
+            reset_timer();
+          }
+        }
+      }
+
+      if (current_size <= current_limit) {
+        current_size = current_limit;
+        if (gate_on) pause_DR = true; // if limit reached with gate on, sustain envelope
+      }
+
+      if (current_size == min_limit) {
+        in_progress_DR = false; // only stop once min limit is reached
+      }
+    }
+
+    void advance_envelope() {
+      if (in_progress_A) {
+        rise();
+      } else if (in_progress_DR) {
+        fall();
+      }
+    }
+};
--- examples/example_rasa3x2/example_rasa3x2.ino
+++ examples/example_rasa3x2/example_rasa3x2.ino
@@ -1,141 +1,76 @@
-#include "/home/thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/hardware/rasa3x2.h"
-#include "/home/thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/EuroStep.h"
+//#include "/home/thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/hardware/rasa3x2.h"
+//#include "/home/thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/EuroStep.h"
+#include "C:/Users/Thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/hardware/rasa3x2.h"
+#include "C:/Users/Thom/Dropbox/Hobbies/Synths/Arduino/devkit/EuroStep/EuroStep.h"
 #include "Envelope.h"
 
 class make_Envelope_ADSR : public EuroStep::EuroStep {
-public:
+  public:
 
-  int attack_rate;
-  int decay_rate;
-  int sustain_level;
-  int release_rate;
-  bool A_in_progress;
-  bool D_in_progress;
-  bool S_in_progress;
-  bool R_in_progress;
-  bool R_end;
-  int envelope_size = 0;
+    Envelope Env1;
+    Envelope Env2;
 
-  // used for switches
-  bool fast_A;
-  bool fast_DR;
-  bool fast_A_2;
-  bool fast_DR_2;
+    ///////////////////////////////////////////////////////////////////////////////
+    /// On clock rise, start a new envelope; on clock fall, start release
+    ///////////////////////////////////////////////////////////////////////////////
 
-  ///////////////////////////////////////////////////////////////////////////////
-  /// On clock rise, start a new envelope; on clock fall, start release
-  ///////////////////////////////////////////////////////////////////////////////
-
-  // STUFF THAT HAPPENS WHEN CLOCK RISES (IF APPLICABLE)
-  void on_clock_rise_do() {
-    envelope_size = 0; // reset the envelope
-    A_in_progress = true; // re-start attack
-    D_in_progress = false;
-    S_in_progress = false;
-    R_in_progress = false;
-    reset_timer();
-  }
-
-  // STUFF THAT HAPPENS WHEN CLOCK FALLS (IF APPLICABLE)
-  void on_clock_fall_do() {
-    A_in_progress = false;
-    D_in_progress = false;
-    S_in_progress = false;
-    R_in_progress = true; // start release
-    reset_timer();
-  }
-
-  ///////////////////////////////////////////////////////////////////////////////
-  /// On each step, check for ADSR phases
-  ///////////////////////////////////////////////////////////////////////////////
-
-  // STUFF THAT HAPPENS EVERY STEP
-  void on_step_do() {
-
-    fast_A = switch_values[0];
-    fast_DR = switch_values[1];
-    fast_A_2 = switch_values[2];
-    fast_DR_2 = switch_values[3];
-
-    attack_rate = pot_values[0]; // attack rate ranges 0-100
-    if(A_in_progress){
-      if(debug) Serial.println ("Attack stage.");
-      if(attack_rate == 0){
-        envelope_size = 4000; // if attack is 0, spike up right away
-      }else{
-        if(get_timer() > attack_rate){
-          if(fast_A){
-            envelope_size += .05 * 4000;
-          }else{
-            envelope_size += .01 * 4000;
-          }
-          reset_timer();
-        }
-      }
-      if(envelope_size >= 4000){
-        envelope_size = 4000; // cap envelope size at 4 volts
-        A_in_progress = false; // stop attack
-        D_in_progress = true;
-      }
+    // STUFF THAT HAPPENS WHEN CLOCK RISES AND FALLS (IF APPLICABLE)
+    void on_clock_rise_do() {
+      Env1.turn_on_gate();
+    }
+    void on_clock_fall_do() {
+      Env1.turn_off_gate();
+    }
+    void on_clock_rise_2_do() {
+      Env2.turn_on_gate();
+    }
+    void on_clock_fall_2_do() {
+      Env2.turn_off_gate();
     }
 
-    sustain_level = pot_values[1] * 4000; // sustain level ranges 0-4000
-    decay_rate = pot_values[2]; // decay rate ranges 0-100
-    if(D_in_progress){
-      if(debug) Serial.println ("Decay stage.");
-      if(decay_rate == 0){
-        envelope_size = sustain_level; // if no decay, drop to sustain
-      }else{
-        if(get_timer() > decay_rate){
-          if(fast_DR){
-            envelope_size -= .05 * 4000;
-          }else{
-            envelope_size -= .01 * 4000;
-          }
-          reset_timer();
-        }
-      }
-      if(envelope_size <= sustain_level){
-        envelope_size = sustain_level;
-        D_in_progress = false;
-        S_in_progress = true;
-      }
-    }
+    ///////////////////////////////////////////////////////////////////////////////
+    /// On each step, check for ADSR phases
+    ///////////////////////////////////////////////////////////////////////////////
 
-    release_rate = decay_rate;
-    if(R_in_progress){
-      if(debug) Serial.println ("Release stage.");
-      if(release_rate = 0){
-        envelope_size = 0; // if no release, drop to 0 right away
-      }else{
-        if(get_timer() > release_rate){
-          if(fast_DR){
-            envelope_size -= .05 * 4000;
-          }else{
-            envelope_size -= .01 * 4000;
-          }
-          reset_timer();
-        }
-      }
-      if(envelope_size <= 0){
-        envelope_size = 0;
-        R_in_progress = false;
-        R_end = true;
-      }
+    // STUFF THAT HAPPENS EVERY STEP
+    void on_step_do() {
+
+      // switches set speed of envelope rise / fall
+      Env1.fast_A = switch_values[0];
+      Env1.fast_DR = switch_values[1];
+      Env2.fast_A = switch_values[2];
+      Env2.fast_DR = switch_values[3];
+
+      // pots determine ATTACK, SUSTAIN, and DECAY/RELEASE (shared)
+      Env1.rate_A = max(1, pot_values[0]);                // attack rate ranges 1-100
+      Env1.sustain_limit = max(1, pot_values[1]) * 40;  // sustain level ranges 40-4000
+      Env1.rate_DR = max(1, pot_values[2]);               // decay rate ranges 1-100
+      Env2.rate_A = max(1, pot_values[3]);
+      Env2.sustain_limit = max(1, pot_values[4]) * 40;
+      Env2.rate_DR = max(1, pot_values[5]);
+
+      // this method handles both rise and fall, with timers
+      //  set up by turn_on_gate() and turn_off_gate()
+      Env1.advance_envelope();
+      Env2.advance_envelope();
+
+      send_to_output(0, Env1.current_size);
+      send_to_output(1, Env2.current_size);
     }
-  }
 };
 
 make_Envelope_ADSR module;  // make the class
 
 // RUNS ONCE
 void setup() {
-//   module.input_mode_is_analog[0] = true;   // if input is readAnalog()
-//   module.enable_clock_events(0);           // treat input 0 as a clock signal (optional)
-//   module.output_mode_is_analog[0] = true;  // send output 0 to DAC
-//   module.output_mode_is_analog[1] = true;  // send output 1 to DAC
-//   module.debug = true;                     // toggle debug
-//   module.start();                          // required to initialise pins
+  module.input_mode_is_analog[0] = false;  // if input is readAnalog()
+  module.input_mode_is_analog[1] = false;
+  module.enable_clock_events(0);  // treat input 0 as a clock signal (optional)
+  module.enable_clock_events_2(1);
+  module.output_mode_is_analog[0] = true;  // send output 0 to DAC
+  module.output_mode_is_analog[1] = true;  // send output 1 to DAC
+  module.debug = false;                    // toggle debug
+  module.start();                          // required to initialise pins
 }
 
 // RUNS EVERY STEP
