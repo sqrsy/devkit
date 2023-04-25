299a7f0f4e1d2f2c3137b18f2b4ed63b966b0678
Fair & Square Synthesis <thom@sqrsy.com>
2023-04-25 12:00:56 +1000

    Add INO file to test compile of YM code

---
+++ EuroStep/chips/chips.ino
@@ -0,0 +1,10 @@
+#include "dummy.h"  // dummy pins to test compile
+#include "ym2612.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
