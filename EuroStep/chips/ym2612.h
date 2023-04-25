0443d833417194a45a4487a94ab91f6f1acd6b87
Fair & Square Synthesis <thom@sqrsy.com>
2023-04-25 16:38:20 +1000

    Re-work YM2612 template as class

--- EuroStep/chips/ym2612.h
+++ EuroStep/chips/ym2612.h
@@ -1,44 +1,43 @@
-int get_bit(char byte, int pos) {
+class YM2612 {
 
-  int result = byte >> pos & 1;
-  return (result);
-}
+public:
 
-void write_ym2612(char byte, bool debug) {
+  bool verbose = true;
+  int data_pin[8] = { PIN_DA0, PIN_DA1, PIN_DA2, PIN_DA3, PIN_DA4, PIN_DA5, PIN_DA6, PIN_DA7 };
 
-  // pass data to chip
-  digitalWrite(PIN_CS, 0);
-  digitalWrite(PIN_DA0, get_bit(byte, 0));
-  digitalWrite(PIN_DA1, get_bit(byte, 1));
-  digitalWrite(PIN_DA2, get_bit(byte, 2));
-  digitalWrite(PIN_DA3, get_bit(byte, 3));
-  digitalWrite(PIN_DA4, get_bit(byte, 4));
-  digitalWrite(PIN_DA5, get_bit(byte, 5));
-  digitalWrite(PIN_DA6, get_bit(byte, 6));
-  digitalWrite(PIN_DA7, get_bit(byte, 7));
-  delayMicroseconds(1);
+  void set_byte(char byte) {
 
-  // enable write mode
-  digitalWrite(PIN_WR, 0);
-  digitalWrite(PIN_RD, 1);
-  delayMicroseconds(5);
+    // pass data to chip
+    digitalWrite(PIN_CS, 0);
+    for (int i = 0; i < 8; i++) {
+      digitalWrite(data_pin[i], get_bit(byte, i));  // write i=0 (DA0) then i=1 (DA1) and so on
+    }
+    delayMicroseconds(1);
 
-  // stop write
-  digitalWrite(PIN_WR, 1);
-  digitalWrite(PIN_RD, 0);
-  delayMicroseconds(5);
+    // enable write mode
+    digitalWrite(PIN_WR, 0);
+    digitalWrite(PIN_RD, 1);
+    delayMicroseconds(5);
 
-  // finish
-  digitalWrite(PIN_CS, 1);
-}
+    // stop write
+    digitalWrite(PIN_WR, 1);
+    digitalWrite(PIN_RD, 0);
+    delayMicroseconds(5);
 
-void set_reg_to_val(char reg_byte, char val_byte, bool debug) {
+    // finish
+    digitalWrite(PIN_CS, 1);
+  }
 
-  // write to register
-  digitalWrite(PIN_A0, 0);
-  write_ym2612(reg_byte, debug);
+  void set_reg_to_val(char reg, char val) {
 
-  // write value
-  digitalWrite(PIN_A1, 1);
-  write_ym2612(val_byte, debug);
-}
+    // write to register
+    digitalWrite(PIN_A0, 0);
+    digitalWrite(PIN_A1, 0);  // set to 1 for chan 4-6
+    set_byte(reg);
+
+    // write value
+    digitalWrite(PIN_A0, 1);
+    digitalWrite(PIN_A1, 0);  // set to 1 for chan 4-6
+    set_byte(val);
+  }
+};
