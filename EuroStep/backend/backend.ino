d624bead7da9adcccc80fd33b4f9d63620e76d70
Fair & Square Synthesis <thom@sqrsy.com>
2024-11-13 17:00:13 +1100

    Split backend code and template documentation

--- EuroStep/backend/backend.ino
+++ EuroStep/backend/backend.ino
@@ -1,4 +1,9 @@
-#include "backend.h"
+#include "power_funcs.h"
+#include "transfer_funcs.h"
+#include "bit_funcs.h"
+#include "map_funcs.h"
+#include "read_funcs.h"
+#include "Timer.h"
 
 void setup() {
   // put your setup code here, to run once:
