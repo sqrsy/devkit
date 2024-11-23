7535260624671e7ebe750b33364329a1c087f548
Fair & Square Synthesis <thom@sqrsy.com>
2024-11-24 09:11:20 +1100

    Tweak hardware constants

--- EuroStep/hardware/rasa3x2.h
+++ EuroStep/hardware/rasa3x2.h
@@ -2,14 +2,14 @@
 /// DEFINE HARDWARE PARAMETERS FOR MODULE
 ///////////////////////////////////////////////////////////////////////////////
 
-#define NUMBER_OF_INPUTS 2
+#define NUMBER_OF_JACKS 2
 #define V_DIVIDER_R1 220
 #define V_DIVIDER_R2 150
-#define PINS_INPUT \
+#define PINS_JACK \
   { A0, A7 }
 
 #define NUMBER_OF_POTS 6
-#define MAX_POT_VOLTAGE 5000
+#define MAX_POT_VOLTAGE 4900
 #define REVERSE_POT true
 #define PINS_POT \
   { A2, A6, A5, A1, A3, A4 }
@@ -18,9 +18,9 @@
 #define PINS_SWITCH \
   { 12, 11, 10, 9 }
 
-#define NUMBER_OF_OUTPUTS 2
-#define PINS_OUTPUT \
-  { -1, -1 }
+#define NUMBER_OF_DIGITAL_OUTPUTS 0
+#define PINS_DIGITAL_OUTPUT \
+  {}
 #define PINS_DAC_A \
   { 4, 5, 6, 7 }
 #define PINS_DAC_B \
