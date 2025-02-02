d7b6b4dbcee910ca146a5779947abc26ac9e8ae1
Fair & Square Synthesis <thom@sqrsy.com>
2025-02-02 21:28:59 +1100

    Add pin config for SN module

---
+++ EuroStep/hardware/sn76489.h
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+/// DEFINE HARDWARE PARAMETERS FOR MODULE
+///////////////////////////////////////////////////////////////////////////////
+
+#define NUMBER_OF_JACKS 2
+#define V_DIVIDER_R1 220
+#define V_DIVIDER_R2 150
+#define PINS_JACK \
+  { A3, A4 }
+
+#define NUMBER_OF_POTS 2
+#define MAX_POT_VOLTAGE 4900
+#define REVERSE_POT true
+#define PINS_POT \
+  { A0, A1 }
+
+#define NUMBER_OF_SWITCHES 1
+#define PINS_SWITCH \
+  { 7 }
+
+#define NUMBER_OF_DIGITAL_OUTPUTS 0
+#define PINS_DIGITAL_OUTPUT \
+  {}
+#define PINS_SN \
+  { 5, 6, 12, 11, 10, 9, 8, 2, 3, 4 }  // WE, CE, D0-D7
