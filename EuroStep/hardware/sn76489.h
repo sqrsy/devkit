461480f678ede6d606d0f8a4b0651a2877dbb2c9
Fair & Square Synthesis <thom@sqrsy.com>
2025-04-19 14:12:21 +1000

    Add dummy values for DAC within SN config

--- EuroStep/hardware/sn76489.h
+++ EuroStep/hardware/sn76489.h
@@ -5,6 +5,8 @@
 #define NUMBER_OF_JACKS 2
 #define V_DIVIDER_R1 220
 #define V_DIVIDER_R2 150
+#define JACK_IS_DIGITAL \
+  { false, false }
 #define PINS_JACK \
   { A3, A4 }
 
@@ -21,5 +23,10 @@
 #define NUMBER_OF_DIGITAL_OUTPUTS 0
 #define PINS_DIGITAL_OUTPUT \
   {}
-#define PINS_SN \
+#define PINS_DAC_A \
+  { -1, -1, -1, -1 }
+#define PINS_DAC_B \
+  { -1, -1, -1, -1 }
+
+#define PINS_SN76489 \
   { 5, 6, 12, 11, 10, 9, 8, 2, 3, 4 }  // WE, CE, D0-D7
